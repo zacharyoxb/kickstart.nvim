7b9c5c4d6a5191e993e7d022a76d0eb113f0d649
zacharyoxb <oxboroughzachary@protonmail.com>
2025-09-06 19:33:16 +0100

    bind jj to esc

--- init.lua
+++ init.lua
@@ -207,6 +207,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Rebind jj to esc
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = false })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
